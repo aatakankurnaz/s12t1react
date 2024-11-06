d97af9b9ebc407d0ecbf29747431a74f92ba68c4
aatakankurnaz <aatakankurnaz@gmail.com>
2024-11-06 03:13:47 +0300

    main foto eklendi

--- s12t1react/src/components/Main.jsx
+++ s12t1react/src/components/Main.jsx
@@ -1,14 +1,14 @@
 export const Main = () => {
     return (
-        <main>
+        <main className="flex mx-20 mt-20">
             <section>
-                <img src="./ProfilePhoto.jpg" alt="FOTO" />
+                <img src="./ProfilePhoto.JPG" alt="FOTO" className="rounded-lg" />
             </section>
             <section>
-                <blockquote></blockquote>
-                <p></p>
-                <p></p>
-                <button></button>
+                <blockquote>Her girişimci, dijital dünyada iz bırakmak için kendi internet sitesine sahip olmalıdır; çünkü bir fikir, paylaşıldığı platformda hayat bulur.</blockquote>
+                <p>Atakan Kurnaz</p>
+                <p>Frontend Developer</p>
+                <button>Fikirlerine Hayat Ver</button>
             </section>
             <section>
                 <ul>
