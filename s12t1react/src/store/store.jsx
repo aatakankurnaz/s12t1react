b217d10e7ecb1a90dc7762b75a3298a732751c15
aatakankurnaz <aatakankurnaz@gmail.com>
2024-11-07 13:09:34 +0300

    locale storage kaydı

--- s12t1react/src/store/store.jsx
+++ s12t1react/src/store/store.jsx
@@ -2,21 +2,23 @@ import { legacy_createStore as createStore } from "redux";
 
 
 const TOGGLE_DARK_MODE = "TOGGLE_DARK_MODE"
-
+const savedDarkMode = localStorage.getItem('darkMode') === 'true';
 export const toggleDarkMode = () => {
-    return {type: TOGGLE_DARK_MODE}
+  return { type: TOGGLE_DARK_MODE }
 }
 const initialState = {
-    isDarkMode : false
+  isDarkMode: savedDarkMode
 }
 
 const darkModeReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case TOGGLE_DARK_MODE:
-        return { ...state, isDarkMode: !state.isDarkMode };
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case TOGGLE_DARK_MODE:
+      const newDarkMode = !state.isDarkMode;
+      localStorage.setItem('darkMode', newDarkMode);
+      return { ...state, isDarkMode: !state.isDarkMode };
+    default:
+      return state;
+  }
+};
 
 export const store = createStore(darkModeReducer)
