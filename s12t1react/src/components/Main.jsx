76195ed2398018b21c501389e88ec113b1f0f060
aatakankurnaz <aatakankurnaz@gmail.com>
2024-11-07 15:19:08 +0300

    ++

--- s12t1react/src/components/Main.jsx
+++ s12t1react/src/components/Main.jsx
@@ -52,7 +52,7 @@ export const Main = () => {
             <div className="flex-1 flex flex-col items-center gap-3">
               <h2>{getLocalizedText('link2')}</h2>
               <nav className="flex flex-col gap-[10px]">
-                <a href="https://vercel.com/aatakankurnazs-projects/pizzareact">{getLocalizedText('project1')}</a>
+                <p>{getLocalizedText('project1')}</p>
                 <p>{getLocalizedText('project2')}</p>
                 <p>{getLocalizedText('project3')}</p>
               </nav>
@@ -60,6 +60,8 @@ export const Main = () => {
           </div>
         </section>
       </main>
+
+      
     </div>
   );
 };
